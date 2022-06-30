a2476170e8eb724ee37229c7659a37e81151f63b
fear4good <83022667+fear4good@users.noreply.github.com>
2022-06-30 17:51:02 +0300

    Delete projectweb directory

--- projectweb/connect.php
+++
@@ -1,8 +0,0 @@
-<?php
-$db = new mysqli('localhost', 'root', '', 'project_web');
-
-if (mysqli_connect_error()) 
-{
-    die('Connect Error (' . mysqli_connect_errno() . ') '. mysqli_connect_error());
-}
-?>
--- projectweb/geojson/readme.txt
+++
@@ -1,3 +0,0 @@
-starting_pois.json 2021-11-29 12:19:00
-generic.json	2021-11-29 12:50:00
-specific.json	2021-11-29 12:49:00
